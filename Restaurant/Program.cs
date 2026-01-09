1dacdba91dc8f23a0dcb781fb3c6c8b0ebe02374
salm0nlover <poom_poomfast@hotmail.com>
2026-01-09 15:40:50 +0700

    Add project files.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,24 @@
+namespace Restaurant
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Bhasoontara Paenohai ID 66102010147
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            if (args.Length > 0 && args[0].ToLower() == "-k")
+                Application.Run(new KitchenForm());
+            else
+                Application.Run(new WaiterForm());
+
+
+
+        }
+    }
+}
